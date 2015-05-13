08b24880c9f1759cffc7cc4c304319ca89b1a2ba
Jon Edwards <jkedwards@me.com>
2015-05-12 22:07:16 -0700

    add isprime and ispalindrome

---
+++ test/plugins/endpoints/puzzles/ispalindrome.js
@@ -0,0 +1,61 @@
+/* eslint no-unused-expressions: 0 */
+'use strict';
+
+var Chai = require('chai');
+var Lab = require('lab');
+var Mongoose = require('mongoose');
+var Server = require('../../../../lib/server');
+
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Chai.expect;
+var it = lab.test;
+
+var before = lab.before;
+var after = lab.after;
+
+var server;
+
+
+describe('GET /puzzles/ispalindrome/{sentence}', function(){
+  before(function(done){
+    Server.init(function(err, srvr){
+      if(err){ throw err; }
+      server = srvr;
+      done();
+    });
+  });
+  after(function(done){
+    server.stop(function(){
+      Mongoose.disconnect(done);
+    });
+  });
+  it('should return true for "a man a plan a canal panama"', function(done){
+    server.inject({method: 'GET', url: '/puzzles/palindrome/a man a plan a canal panama', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(true);
+      done();
+    });
+  });
+  it('should return false for "sarah"', function(done){
+    server.inject({method: 'GET', url: '/puzzles/palindrome/sarah', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(false);
+      done();
+    });
+  });
+  it('should return false for "hannah"', function(done){
+    server.inject({method: 'GET', url: '/puzzles/palindrome/hannah', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(true);
+      done();
+    });
+  });
+  it('should return false for "not so boston"', function(done){
+    server.inject({method: 'GET', url: '/puzzles/palindrome/hannah', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(true);
+      done();
+    });
+  });
+});
---
+++ test/plugins/endpoints/puzzles/isprime.js
@@ -0,0 +1,54 @@
+/* eslint no-unused-expressions: 0 */
+'use strict';
+
+var Chai = require('chai');
+var Lab = require('lab');
+var Mongoose = require('mongoose');
+var Server = require('../../../../lib/server');
+
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Chai.expect;
+var it = lab.test;
+
+var before = lab.before;
+var after = lab.after;
+
+var server;
+
+
+describe('GET /puzzles/isprime/{num}', function(){
+  before(function(done){
+    Server.init(function(err, srvr){
+      if(err){ throw err; }
+      server = srvr;
+      done();
+    });
+  });
+  after(function(done){
+    server.stop(function(){
+      Mongoose.disconnect(done);
+    });
+  });
+  it('should return true for 199', function(done){
+    server.inject({method: 'GET', url: '/puzzles/isprime/199', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(true);
+      done();
+    });
+  });
+  it('should return true for 16777213', function(done){
+    server.inject({method: 'GET', url: '/puzzles/isprime/16777213', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(true);
+      done();
+    });
+  });
+  it('should return true for 8192', function(done){
+    server.inject({method: 'GET', url: '/puzzles/isprime/8192', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.equal(false);
+      done();
+    });
+  });
+});
